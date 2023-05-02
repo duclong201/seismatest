0a4ed712a0a4b0b920103e1e04d90718e377158c
Long Nguyen <61676915+duclong201@users.noreply.github.com>
2023-05-02 17:19:23 +1000

    Revert "Remove utils folder"
    This reverts commit 0a01ac23947dfdddd55bb086007762898af2776d.

--- application.go
+++ application.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"main/utils"
 	"math"
 	"mime/multipart"
 	"net/http"
@@ -70,12 +71,12 @@ func HandleUpload(c *gin.Context) {
 }
 
 // Handle CSV file, calculate tax and return payslips for given CSV file
-func HandleCSVFile(file multipart.File) ([]PaySlip, error) {
+func HandleCSVFile(file multipart.File) ([]utils.PaySlip, error) {
 	csvLines, err := csv.NewReader(file).ReadAll()
 	if err != nil {
 		return nil, err
 	}
-	var payslips []PaySlip
+	var payslips []utils.PaySlip
 
 	for i, line := range csvLines {
 		if i == 0 {
@@ -92,7 +93,7 @@ func HandleCSVFile(file multipart.File) ([]PaySlip, error) {
 }
 
 // Handle Json file, calculate tax and return payslips for given JSON file
-func HandleJSONFile(file multipart.File) ([]PayslipResponse, error) {
+func HandleJSONFile(file multipart.File) ([]utils.PayslipResponse, error) {
 	data, err := ioutil.ReadAll(file)
 	if err != nil {
 		return nil, err
@@ -104,10 +105,10 @@ func HandleJSONFile(file multipart.File) ([]PayslipResponse, error) {
 		return nil, err
 	}
 
-	var payslips []PayslipResponse
+	var payslips []utils.PayslipResponse
 
 	for _, obj := range jsonData {
-		employee := Employee{FirstName: obj["firstName"].(string),
+		employee := utils.Employee{FirstName: obj["firstName"].(string),
 			LastName:     obj["lastName"].(string),
 			AnnualSalary: obj["annualSalary"].(float64),
 			PaymentMonth: int(obj["paymentMonth"].(float64)),
@@ -121,12 +122,12 @@ func HandleJSONFile(file multipart.File) ([]PayslipResponse, error) {
 
 // Handle request to calculate tax
 func HandleRequest(c *gin.Context) {
-	var employees []Employee
+	var employees []utils.Employee
 	if err := c.ShouldBindJSON(&employees); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	var payslips []PayslipResponse
+	var payslips []utils.PayslipResponse
 	for _, employee := range employees {
 		payslip := GenerateRESTPayslip(employee)
 		payslips = append(payslips, payslip)
@@ -138,8 +139,8 @@ func HandleRequest(c *gin.Context) {
 }
 
 // Generate payslip for given employee
-func GenerateRESTPayslip(employee Employee) PayslipResponse {
-	var payslip PayslipResponse
+func GenerateRESTPayslip(employee utils.Employee) utils.PayslipResponse {
+	var payslip utils.PayslipResponse
 	payslip.Employee = employee
 	payslip.GrossIncome = int(employee.AnnualSalary)
 	incomeTax := CalculateTax(employee.AnnualSalary)
@@ -162,23 +163,23 @@ func lastDayOfCurrentMonth() string {
 }
 
 // Parse employee for given line from the csv file
-func ParseEmployeeCSV(line []string) (CSVEmployee, error) {
+func ParseEmployeeCSV(line []string) (utils.CSVEmployee, error) {
 	annualSalary, err := strconv.ParseFloat(line[2], 64)
 	if err != nil {
-		return CSVEmployee{}, err
+		return utils.CSVEmployee{}, err
 	}
 
 	superRate, err := strconv.ParseFloat(line[3], 64)
 	if err != nil {
-		return CSVEmployee{}, err
+		return utils.CSVEmployee{}, err
 	}
 
-	return CSVEmployee{FirstName: line[0], LastName: line[1], AnnualSalary: annualSalary, PaymentStart: line[4], SuperRate: superRate}, nil
+	return utils.CSVEmployee{FirstName: line[0], LastName: line[1], AnnualSalary: annualSalary, PaymentStart: line[4], SuperRate: superRate}, nil
 }
 
 // Generate Payslip for given employee
-func GenerateCSVPayslip(employee CSVEmployee) PaySlip {
-	var payslip PaySlip
+func GenerateCSVPayslip(employee utils.CSVEmployee) utils.PaySlip {
+	var payslip utils.PaySlip
 	payslip.Name = employee.FirstName + " " + employee.LastName
 	payslip.AnnualSalary = employee.AnnualSalary
 	payslip.IncomeTax = CalculateTax(employee.AnnualSalary)
@@ -189,9 +190,9 @@ func GenerateCSVPayslip(employee CSVEmployee) PaySlip {
 }
 
 // Load TaxRates from csv file
-func GetTaxRates() []TaxRate {
+func GetTaxRates() []utils.TaxRate {
 	// Provide default tax rates
-	defaultTaxRates := []TaxRate{{0, 18200, 0, 0},
+	defaultTaxRates := []utils.TaxRate{{0, 18200, 0, 0},
 		{18200, 37000, 0, 0.19},
 		{37000, 87000, 3572, 0.325},
 		{87000, 180000, 19822, 0.37},
@@ -210,7 +211,7 @@ func GetTaxRates() []TaxRate {
 		fmt.Println(err)
 	}
 
-	var taxRates []TaxRate
+	var taxRates []utils.TaxRate
 
 	for i, line := range csvLines {
 		if i == 0 {
@@ -227,8 +228,8 @@ func GetTaxRates() []TaxRate {
 }
 
 // Parse Tax rate from read csv line
-func ParseTaxRates(line []string) (TaxRate, error) {
-	defaultTaxRate := TaxRate{Bracket: 0, MaxValue: 0, FixValue: 0, Rate: 0}
+func ParseTaxRates(line []string) (utils.TaxRate, error) {
+	defaultTaxRate := utils.TaxRate{Bracket: 0, MaxValue: 0, FixValue: 0, Rate: 0}
 	bracket, err := strconv.ParseFloat(line[0], 64)
 	if err != nil {
 		return defaultTaxRate, err
@@ -249,7 +250,7 @@ func ParseTaxRates(line []string) (TaxRate, error) {
 	if err != nil {
 		return defaultTaxRate, err
 	}
-	return TaxRate{Bracket: bracket, MaxValue: maxValue, FixValue: fixValue, Rate: rate}, nil
+	return utils.TaxRate{Bracket: bracket, MaxValue: maxValue, FixValue: fixValue, Rate: rate}, nil
 }
 
 // CalculateTax method calculates the income tax for the given annual salary
